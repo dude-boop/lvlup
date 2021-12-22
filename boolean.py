a3daed7f43644a2c729b7cb60f9352d77fcd2de6
dude-boop <psihnofrost@gmail.com>
2021-12-22 21:02:25 +0200

    boolean.py was added

---
+++ boolean.py
@@ -0,0 +1,20 @@
+# print(bool(0))
+# print(bool(''))
+# print(bool(None))
+#
+# print(bool(1*(10**-324)))
+#
+# print(True or False)
+# print(True or True)
+# print(False or False)
+#
+#
+# print(True and False)
+# print(True and True)
+# print(False and False)
+#### or +   and *
+
+## тип данных NotImplemented
+# print(bool(NotImplemented))
+my_var = Ellipsis
+print(bool(my_var))
