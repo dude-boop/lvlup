b0f3844f4d192f1536504e61405b3453b2ded688
dude-boop <psihnofrost@gmail.com>
2022-01-11 21:52:37 +0200

    new_way delete all branches

--- 6/hw_6_list.py
+++ 6/hw_6_list.py
@@ -1,9 +1,36 @@
 """
-существуют ли єлементы одного списка во втором списке
-выбрать оптимальный вариант
+    существуют ли єлементы одного списка во втором списке
+    выбрать оптимальный вариант
 """
+list_hw_6 = ['bool', 'dir', 'list', 'merge', 'mkdir']
+list_hw_6_2 = ['dir']
+print(list_hw_6_2.index("dir"))
+if list_hw_6_2[0] in list_hw_6:
+    print(list_hw_6_2)
 
 """
     'append', 'clear', 'copy', 'count', 'extend', 
     'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
 """
+
+# list_hw_6.append("lsblk")
+# print(list_hw_6)
+# copy_list_hw_6 = list_hw_6.copy
+# print("id: ",id(list_hw_6), "id copy: ", id(copy_list_hw_6))
+# print("how many 'dir' in list: ", list_hw_6.count("dir"))
+# dir = ["Virginia", "Classic", "Maren-go"]
+# print(dir.extend(list_hw_6))
+# print(dir)
+# print(list_hw_6.index("merge"))
+# list_hw_6.insert(0, "string")
+# print(list_hw_6)
+# list_hw_6.pop(0)
+# print(list_hw_6)
+# list_hw_6.remove("bool")
+# print(list_hw_6)
+# list_hw_6.reverse()
+# print(list_hw_6)
+# dir.clear()
+# print(dir)
+# list_hw_6.sort()
+# print(list_hw_6)
